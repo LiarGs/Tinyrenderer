59e9e3c7d442eb9a7726d77cb37c8d881ae5bb13
PatrickStar_bro <15153686+patrickstarbro@user.noreply.gitee.com>
2025-01-04 14:50:04 +0800

    初期准备

---
+++ Base/Log.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include <time.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+static std::string getTime()
+{
+    const char *time_fmt = "%Y-%m-%d %H:%M:%S";
+    time_t t = time(nullptr);
+    char time_str[64];
+    struct tm time_info;
+    localtime_s(&time_info, &t);
+    strftime(time_str, sizeof(time_str), time_fmt, &time_info);
+
+    return time_str;
+}
+
+static std::string getFile(std::string file)
+{
+#ifndef WIN32
+    std::string pattern = "/";
+#else
+    std::string pattern = "\\";
+#endif // !WIN32
+
+    std::string::size_type pos;
+    std::vector<std::string> result;
+    file += pattern; // 扩展字符串以方便操作
+    size_t file_size = file.size();
+    for (size_t i = 0; i < file_size; i++)
+    {
+        pos = file.find(pattern, i);
+        if (pos < file_size)
+        {
+            std::string s = file.substr(i, pos - i);
+            result.push_back(s);
+            i = pos + pattern.size() - 1;
+        }
+    }
+    return result.back();
+}
+
+// 以下是编译器预定义宏
+//  __FILE__ 获取源文件的相对路径和名字
+//  __LINE__ 获取该行代码在文件中的行号
+//  __func__ 或 __FUNCTION__ 获取函数名
+
+// #define LOGI(format_str, ...) std::cerr << "[INFO]" << getTime() << " [" << getFile(__FILE__) << ":" << __LINE__ << "] " << format_str << "\n", ##__VA_ARGS__
+// #define LOGE(format_str, ...) std::cerr << "[ERROR]" << getTime() << " [" << getFile(__FILE__) << ":" << __LINE__ << "] " << format_str << "\n", ##__VA_ARGS__
+
+#define LOGI(format_str, ...) fprintf(stderr, "[INFO]%s [%s:%d] " format_str "\n", getTime().data(), getFile(__FILE__), __LINE__, ##__VA_ARGS__)
+#define LOGE(format_str, ...) fprintf(stderr, "[ERROR]%s [%s:%d] " format_str "\n", getTime().data(), getFile(__FILE__), __LINE__, ##__VA_ARGS__)
---
+++ Base/tgaimage.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include <cstdint>
+#include <fstream>
+#include <vector>
+#include "Log.h"
+#pragma pack(push, 1)
+// TGAHeader 结构体用于表示 TGA 图像的文件头
+struct TGAHeader
+{
+    std::uint8_t idlength = 0; // 标识符长度
+    std::uint8_t colormaptype = 0; // 调色板类型
+    std::uint8_t datatypecode = 0; // 数据类型码
+    std::uint16_t colormaporigin = 0; // 调色板原点
+    std::uint16_t colormaplength = 0; // 调色板长度
+    std::uint8_t colormapdepth = 0; // 调色板深度
+    std::uint16_t x_origin = 0; // X 起始坐标
+    std::uint16_t y_origin = 0; // Y 起始坐标
+    std::uint16_t width = 0; // 图像宽度
+    std::uint16_t height = 0; // 图像高度
+    std::uint8_t bitsperpixel = 0; // 每像素位数
+    std::uint8_t imagedescriptor = 0; // 图像描述符
+};
+#pragma pack(pop)
+
+// TGAColor 结构体用于表示 TGA 图像的像素颜色
+struct TGAColor
+{
+    TGAColor() = default;
+    TGAColor(const std::uint8_t R, const std::uint8_t G, const std::uint8_t B, const std::uint8_t A, const std::uint8_t bpp) : bgra{B, G, R, A}, bytespp(bpp) {}
+    std::uint8_t bgra[4] = {0, 0, 0, 0}; // 蓝色、绿色、红色和 Alpha 通道
+    std::uint8_t bytespp = 4; // 每像素字节数
+    std::uint8_t &operator[](const int i) { return bgra[i]; }
+};
+
+// TGAImage 结构体用于表示 TGA 图像
+class TGAImage
+{
+public:
+    // 图像格式
+    enum Format
+    {
+        GRAYSCALE = 1,
+        RGB = 3,
+        RGBA = 4
+    };
+
+    TGAImage() = default;
+    TGAImage(const int w, const int h, const int bpp);
+    bool read_tga_file(const std::string& filename);
+    bool write_tga_file(const std::string& filename, const bool vflip = true, const bool rle = true) const;
+    void flip_horizontally(); // 水平翻转
+    void flip_vertically(); // 垂直翻转
+    TGAColor get(const int x, const int y) const;
+    void set(const int x, const int y, const TGAColor &c);
+    int width() const;
+    int height() const;
+
+private:
+    bool load_rle_data(std::ifstream &in);
+    bool unload_rle_data(std::ofstream &out) const;
+
+    int w = 0;
+    int h = 0;
+    std::uint8_t bpp = 0; // 每像素字节数
+    std::vector<std::uint8_t> data = {}; // 图像数据
+};
